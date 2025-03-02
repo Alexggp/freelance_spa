7b8698667fe42c9c05ac02fb33134658bf7ed4d1
Alejandro <alex.ggperez@gmail.com>
2025-03-02 18:24:06 +0100

    main page text on global.json

--- src/pages/Main/MainBody/MainBody.jsx
+++ src/pages/Main/MainBody/MainBody.jsx
@@ -1,45 +1,47 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 import classes from './MainBody.module.css';
 import Container from '../../../components/Container/Container';
-import FaroImg from '../../../assets/faro-azul.png'
+import FaroImg from '../../../assets/faro-azul.png';
 import Button from '../../../components/Button/Button';
 import Logo from '../../../components/Logo/Logo';
 
-
-
 const MainBody = () => {
   const { t } = useTranslation('global');
+  const navigate = useNavigate();
+
+  const navigateTo = () => {
+    navigate('/more');
+  };
+
   return (
     <div className={classes.MainBody}>
-
       <Container className={classes.BodyContainer} size={'lg'}>
         <div className={classes.MainText}>
-          <p>Tienes que tener una cosa bien clara.</p>
-          <p>Una <span>empresa</span> sin una buena identidad digital es un barco a la deriva, en la noche, con las <span>luces apagadas</span>.</p>
-          <p>Nadie te ve, nadie sale en tu busca y lo peor...</p>
-          <p>Te vas <span>a pique</span>.</p>
-          <p>Tranquilo, no dejes que eso te hunda.</p>
-          <p>Internet nos abre un océano infinito<br />de <span>oportunidades</span>.</p>
-          <p>Lleno de posibilidades, lleno de <span>tesoros</span>.</p>
-          <p>Pero hay demasiados peces en el mar...</p>
-          <p>¿Qué <span>tecnología</span> escoger? ¿Qué <span>proveedor</span> contratar?</p>
-          <img className={classes.Image} src={FaroImg}/>
+          {t('mainBody.intro', { returnObjects: true }).map((text, index) => (
+            <p key={index} dangerouslySetInnerHTML={{ __html: text }} />
+          ))}
+          {t('mainBody.ocean', { returnObjects: true }).map((text, index) => (
+            <p key={index} dangerouslySetInnerHTML={{ __html: text }} />
+          ))}
+          <img className={classes.Image} src={FaroImg} alt="Faro" />
         </div>
         <div className={classes.CtaText}>
-          <p>No te dejes seducir por sirenas que te llevan contra las rocas.</p>
-          <h3><span>Pisa en tierra firme</span></h3>
-          <Button className={classes.Button}>Contáctanos</Button>
+          <p>{t('mainBody.cta.warning')}</p>
+          <h3><span>{t('mainBody.cta.action')}</span></h3>
+          <Button className={classes.Button}>{t('mainBody.cta.button')}</Button>
         </div>
       </Container>
       <div className={classes.Footer}>
-          <Logo />
-          <div className={classes.enlaceSubrayado}>Quiero profundizar</div>
+        <Logo />
+        <div onClick={navigateTo} className={classes.enlaceSubrayado}>
+          {t('mainBody.footer.explore')}
         </div>
+      </div>
     </div>
   );
-
-}
+};
 
 export default MainBody;
