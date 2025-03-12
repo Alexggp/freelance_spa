1a4d6522a7092d73e030dcb55dc8b4a8f9cece73
Alejandro <alex.ggperez@gmail.com>
2025-03-12 13:11:25 +0100

    Faro con mascara

--- src/pages/Main/MainBody/MainBody.jsx
+++ src/pages/Main/MainBody/MainBody.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 import classes from './MainBody.module.css';
 import Container from '../../../components/Container/Container';
-import FaroImg from '../../../assets/faro-azul.png';
+import LighHouse from './LightHouse/LightHouse';
 import Button from '../../../components/Button/Button';
 import Logo from '../../../components/Logo/Logo';
 
@@ -26,7 +26,9 @@ const MainBody = () => {
           {t('mainBody.ocean', { returnObjects: true }).map((text, index) => (
             <p key={index} dangerouslySetInnerHTML={{ __html: text }} />
           ))}
-          <img className={classes.Image} src={FaroImg} alt="Faro" />
+          <div className={classes.Image}>
+            <LighHouse />
+          </div>
         </div>
         <div className={classes.CtaText}>
           <p>{t('mainBody.cta.warning')}</p>
