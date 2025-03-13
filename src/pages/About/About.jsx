f6601e2220e815488531f5486b0d398c6f38dca6
Alejandro <alex.ggperez@gmail.com>
2025-03-13 17:38:04 +0100

    sounds

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import classes from './About.module.css';
 import AboutBody from './AboutBody/AboutBody';
 import AboutIntro from './AboutIntro/AboutIntro';
+import hopeSound from '../../assets/sounds/hope.mp3';
+import SoundPlayer from '../../components/SoundPlayer/SoundPlayer';
 
 const About = () => {
 
   return (
     <div className={classes.About}>
+      <SoundPlayer sound={hopeSound}/>
      <AboutIntro/>
      <div className={classes.Body}><AboutBody/></div>
     </div>
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import Lenis from 'lenis';
-import 'lenis/dist/lenis.css'
 
 import classes from './Main.module.css';
 import VideoBg from '../../assets/video_background.mp4';
+import waveSound from '../../assets/sounds/sea-waves.mp3';
 import Logo from '../../components/Logo/Logo';
 import Button from '../../components/Button/Button';
 import MainBody from './MainBody/MainBody';
-
+import SoundPlayer from '../../components/SoundPlayer/SoundPlayer';
 
 
 const Main = () => {
   const { t } = useTranslation('global');
-  // Initialize Lenis
-const lenis = new Lenis({
-  autoRaf: true,
-});
   return (
     <div className={classes.MainPage}>
-
+      <SoundPlayer sound={waveSound}/>
       <div className={classes.BackgroundContainer}>
         <video autoPlay loop muted className={classes.Video}>
           <source src={VideoBg} type="video/mp4" />
