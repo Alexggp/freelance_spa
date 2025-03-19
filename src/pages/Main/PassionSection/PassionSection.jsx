0a90e282b82a3b22d641d0d6b99868f0595be5c6
Alejandro <alex.ggperez@gmail.com>
2025-03-19 09:38:17 +0100

    Passion Section

---
+++ src/pages/Main/PassionSection/PassionSection.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import classes from './PassionSection.module.css';
+import { useTranslation } from 'react-i18next';
+import PhotoShooter from '../../../components/PhotoShooter/PhotoShooter';
+import useScreenSize from '../../../hooks/useScreenSize';
+import { useCursor } from '../../../contexts/CursorContext';
+
+const PassionSection = () => {
+  const { t } = useTranslation('global');
+  const {isDesktop} = useScreenSize();
+  const { setCursorType } = useCursor();
+
+
+  return (
+    <>
+      <div 
+      className={classes.PassionSection}
+      onMouseEnter={() => setCursorType("heart")}
+      onMouseLeave={() => setCursorType("default")}
+      >
+      <PhotoShooter fixed={!isDesktop}/>
+      <p className={classes.Smile}>{t('main.PassionSection.smile')}</p>
+      <div className={classes.Question}>
+        <span>{t('main.PassionSection.question1').toUpperCase()}</span><br/>
+        <span>{t('main.PassionSection.question2').toUpperCase()}</span>
+      </div>
+      
+    </div>
+    <div className={classes.DarkSection}></div>
+    </>
+
+  );
+
+}
+
+export default PassionSection;
