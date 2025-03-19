f1d84242c55e8182f6f40362c31d188373e78858
Alejandro <alex.ggperez@gmail.com>
2025-03-19 16:10:15 +0100

    Inspiration without parallax

--- src/pages/Main/Inspiration/Inspiration.jsx
+++ src/pages/Main/Inspiration/Inspiration.jsx
@@ -5,17 +5,23 @@ import { useTranslation } from 'react-i18next';
 import Image1 from '../../../assets/backgrounds/inspiration1.jpg';
 import Image2 from '../../../assets/backgrounds/inspiration2.jpg';
 import ImageDiscover from '../../../components/ImageDiscover/ImageDiscover';
-
+import { useCursor } from '../../../contexts/CursorContext';
 
 const Inspiration = () => {
-  const { t } = useTranslation('global');
+  
+  const { t } = useTranslation('global');  
+  const { setCursorType } = useCursor();
+  
   return (
     <div className={classes.Inspiration}>
-      <div className={classes.ImagesContainer}>
-      <ImageDiscover baseImage={Image1} overlayImage={Image2} />
+      <div className={classes.ImagesContainer}
+        onMouseEnter={() => setCursorType("none")}
+        onMouseLeave={() => setCursorType("default")}
+      >
+        <h1 className={classes.Title}>{t('main.Inspiration.title')}</h1>
+        <ImageDiscover baseImage={Image1} overlayImage={Image2} />
       </div>
-      <p className={classes.Title}>{t('main.Inspiration.title')}</p>
-      <p className={classes.Title}>{t('main.Inspiration.subtitle')}</p>
+      <div className={classes.Subtitle}><p>{t('main.Inspiration.subtitle')}</p></div>
 
     </div>
   );
