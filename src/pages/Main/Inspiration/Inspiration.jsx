d780e876709971e7d0e820f3f392405a4df89259
Alejandro <alex.ggperez@gmail.com>
2025-03-19 16:21:50 +0100

    Parallax on Inspiration Section

--- src/pages/Main/Inspiration/Inspiration.jsx
+++ src/pages/Main/Inspiration/Inspiration.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Inspiration.module.css';
 import { useTranslation } from 'react-i18next';
+import { Parallax, Background } from 'react-parallax';
 
 import Image1 from '../../../assets/backgrounds/inspiration1.jpg';
 import Image2 from '../../../assets/backgrounds/inspiration2.jpg';
@@ -8,22 +9,26 @@ import ImageDiscover from '../../../components/ImageDiscover/ImageDiscover';
 import { useCursor } from '../../../contexts/CursorContext';
 
 const Inspiration = () => {
-  
-  const { t } = useTranslation('global');  
+
+  const { t } = useTranslation('global');
   const { setCursorType } = useCursor();
-  
+
   return (
     <div className={classes.Inspiration}>
-      <div className={classes.ImagesContainer}
-        onMouseEnter={() => setCursorType("none")}
-        onMouseLeave={() => setCursorType("default")}
-      >
-        <h1 className={classes.Title}>{t('main.Inspiration.title')}</h1>
-        <ImageDiscover baseImage={Image1} overlayImage={Image2} />
-      </div>
+      <Parallax className={classes.ParallaxContainer} strength={200}>
+        <Background className={classes.CustomBackground}>
+          <div className={classes.ImagesContainer}
+            onMouseEnter={() => setCursorType("none")}
+            onMouseLeave={() => setCursorType("default")}
+          >
+            <h1 className={classes.Title}>{t('main.Inspiration.title')}</h1>
+            <ImageDiscover baseImage={Image1} overlayImage={Image2} />
+          </div>
+        </Background>
+      </Parallax>
       <div className={classes.Subtitle}><p>{t('main.Inspiration.subtitle')}</p></div>
 
-    </div>
+    </div >
   );
 
 }
