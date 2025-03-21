02e0d1f03000a61da7f20e27a792cc0e851db720
Alejandro <alex.ggperez@gmail.com>
2025-03-21 18:23:30 +0100

    menu

--- src/pages/RootLayout/Menu/Menu.jsx
+++ src/pages/RootLayout/Menu/Menu.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
 import { useCursor } from '../../../contexts/CursorContext';
 import { useNavigate } from 'react-router-dom';
-
+import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
 
 import Footer from '../Footer/Footer';
 import classes from './Menu.module.css';
 
-const Menu = ({isVisible}) => {
+const Menu = ({ isVisible }) => {
+  const { i18n, t } = useTranslation('global');
   const { setCursorType } = useCursor();
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-  const navigateTo = () => {
-    navigate('/');
+
+  const handleClick = (locale) => {
+    if (locale !== i18n.language){
+      i18n.changeLanguage(locale);
+    }
+  }
+
+
+  const navigateTo = (url) => {
+    navigate('/'+url);
   };
 
+  const Selectable = ({ children, onClick, className }) => (
+    <div className={`${classes.Selectable} ${className}`}
+      onClick={onClick}
+      onMouseEnter={() => setCursorType("pointer")}
+      onMouseLeave={() => setCursorType("default")}
+    >{children}</div>
+  )
 
   return (
     <div className={`${classes.Menu} ${isVisible && classes.isVisible}`}>
-      <p>Sobre mí & Proyectos</p>
-      <p>Contacto</p>
-      <p>Español - Ingles</p>
-      <Footer/>
+      {pathname!=='/' && <Selectable onClick={()=>navigateTo('')}>{t('root.Menu.home')}</Selectable>}
+      {pathname!=='about' && <Selectable onClick={()=>navigateTo('about')}>{t('root.Menu.about')}</Selectable>}
+      {pathname!=='contact' && <Selectable onClick={()=>navigateTo('contact')}>{t('root.Menu.contact')}</Selectable>}
+      <div className={classes.LanguageSelector}>
+        <Selectable className={i18n.language === 'es' ? classes.LocaleSelected : ''} onClick={()=>handleClick('es')}>{t('root.Menu.locales.spanish')}</Selectable>
+        -
+        <Selectable className={i18n.language === 'en' ? classes.LocaleSelected : ''} onClick={()=>handleClick('en')}>{t('root.Menu.locales.english')}</Selectable>
+      </div>
+      <Footer />
     </div>
   );
 
