57c5f6f232fc340280aecc05f9aa7dca572d4e36
Alejandro <alex.ggperez@gmail.com>
2025-03-21 10:44:15 +0100

    header finished

--- src/pages/RootLayout/Header/Header.jsx
+++ src/pages/RootLayout/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import classes from './Header.module.css';
-import CustomIcon from '../../../components/CustomIcon/CustomIcon';
 import Logo from '../Logo/Logo';
-import MenuIcon from '../../../assets/icons/svg/menu.svg?react';
+import MenuIcon from '../../../assets/icons/svg/menu.svg';
+import { useCursor } from '../../../contexts/CursorContext';
 
-const Header = () => {
+const Header = ({toggleMenu}) => {
   const [isVisible, setIsVisible] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const { setCursorType } = useCursor();
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
-      
+
       if (currentScrollY > lastScrollY && currentScrollY > 50) {
         // Scrolling Down - Oculta el Header
         setIsVisible(false);
@@ -24,19 +25,25 @@ const Header = () => {
     };
 
     window.addEventListener('scroll', handleScroll);
-    
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, [lastScrollY]);
 
   return (
-    <div className={`${classes.Header} ${isVisible ? classes.visible : classes.hidden}`}>
+    <header className={`${classes.Header} ${isVisible ? classes.visible : classes.hidden}`}>
       <div className={classes.Box}>
         <Logo />
-        <CustomIcon Icon={MenuIcon} onClick={() => alert('menu')} />
+        <button className={classes.IconContainer}
+          onClick={toggleMenu}
+          onMouseEnter={() => setCursorType("pointer")}
+          onMouseLeave={() => setCursorType("default")}
+        >
+          <img src={MenuIcon}/>
+        </button>
       </div>
-    </div>
+    </header>
   );
 };
 
