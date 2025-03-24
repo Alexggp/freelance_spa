dbb79c6a733991a4f4122f940ef12f76657d33f2
Alejandro <alex.ggperez@gmail.com>
2025-03-24 08:34:15 +0100

    about CTA a11y

--- src/pages/Main/Introduction/AboutCta/AboutCta.jsx
+++ src/pages/Main/Introduction/AboutCta/AboutCta.jsx
@@ -5,30 +5,32 @@ import { useTranslation } from 'react-i18next';
 import classes from './AboutCta.module.css';
 import { useCursor } from '../../../../contexts/CursorContext';
 import ctaImage from '../../../../assets/icons/svg/enter.svg';
+
 const AboutCta = () => {
   const { t } = useTranslation('global');
-    const { setCursorType } = useCursor();
-    const navigate = useNavigate();
+  const { setCursorType } = useCursor();
+  const navigate = useNavigate();
 
-    const navigateTo = () => {
-      navigate('/about');
-    };
+  const navigateTo = () => {
+    navigate('/about');
+  };
+
   return (
-    <button className={classes.AboutCta}
-    onClick={navigateTo}
-    onMouseEnter={() => setCursorType("pointer")}
-    onMouseLeave={() => setCursorType("default")}
-  >
-    <div className={classes.Container}>
-      <div className={classes.Title}>{t('main.Introduction.cta')}</div>
-      <div className={classes.IconContainer}>
-        <img src={ctaImage} alt={t('main.Introduction.cta')}/>
+    <button
+      className={classes.AboutCta}
+      onClick={navigateTo}
+      onMouseEnter={() => setCursorType("pointer")}
+      onMouseLeave={() => setCursorType("default")}
+      aria-label={t('main.Introduction.cta')}
+    >
+      <div className={classes.Container}>
+        <div className={classes.Title}>{t('main.Introduction.cta')}</div>
+        <div className={classes.IconContainer}>
+          <img src={ctaImage} alt={t('main.Introduction.cta')} />
+        </div>
       </div>
-    </div>
-
     </button>
   );
-
 }
 
 export default AboutCta;
