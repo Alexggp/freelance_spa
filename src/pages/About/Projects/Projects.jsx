003642c0c4df9f966fe96204743d3a8abb35f4e6
Alejandro <alex.ggperez@gmail.com>
2025-03-25 10:18:18 +0100

    projects mobile

--- src/pages/About/Projects/Projects.jsx
+++ src/pages/About/Projects/Projects.jsx
@@ -1,36 +1,50 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import classes from './Projects.module.css';
 import { useCursor } from '../../../contexts/CursorContext';
-
+import useScreenSize from '../../../hooks/useScreenSize';
 import ProjectReview from './ProjectReview/ProjectReview';
 
 const Projects = () => {
   const { t } = useTranslation('projects');
   const [activeProject, setActiveProject] = useState(0);
   const { setCursorType } = useCursor();
-
+  const { isDesktop } = useScreenSize();
   const projects = t('projects.projects', { returnObjects: true });
 
 
-  const handleEnter=(p)=>{
+  const handleEnter = (p) => {
     setCursorType("pointer")
     setActiveProject(p);
   }
 
-  const handleLeave=()=>{
+  const handleLeave = () => {
     setCursorType("default")
     setActiveProject(null);
+  }
+
+  const handleClick = (p) => {
+    if (isDesktop) return;
+    setActiveProject(p);
   }
 
   const projectsList = projects.map((project, index) => {
     return (
       <li key={index}
-        className={activeProject?.index === index ? classes.Active : null}
+        className={activeProject?.title === project.title ? classes.Active : null}
         onMouseEnter={() => handleEnter(project)}
+        onClick={() => handleClick(project)}
         onMouseLeave={() => handleLeave()}
       >
         {project.title}
+
+        {!isDesktop ?
+          <div className={`${classes.InfoContainerMobile} ${activeProject?.title === project.title ? classes.Active : null}`}>
+            <ProjectReview project={project} />
+          </div>
+
+          : null
+        }
       </li>
     );
   });
